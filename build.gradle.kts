09668026c72c0f39da2d7396732bd3a0cc207739
Liz Ainslie <lizainslie16@gmail.com>
2023-11-28 18:22:27 -0700

    feat(init): gradle

---
+++ build.gradle.kts
@@ -0,0 +1,17 @@
+plugins {
+    kotlin("multiplatform") version "1.9.20"
+}
+
+group = "system.alpha"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+kotlin {
+    @OptIn(org.jetbrains.kotlin.gradle.targets.js.dsl.ExperimentalWasmDsl::class)
+    wasmJs {
+        binaries.library()
+    }
+}
